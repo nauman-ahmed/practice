0bcb97e479c63df9c04b5dcdefdfe598da943c99
Nauman Ahmed <naumanahmed449@gmail.com>
2024-12-09 23:36:13 +0100

    challenge 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
-import UserCard from './components/card'
+import Counter from './components/card'
 
 function App() {
 
   return (
     <>
       <div className='min-h-screen flex items-center justify-center'>
-        <UserCard name="John Doe" age={30} isActive={true} />
-        <UserCard name="Jane Smith" />
-        <UserCard name="Alice Johnson" isActive={false} />
+        <Counter />
       </div>
     </>
   )
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,19 +1,21 @@
-type TUser = {
-    name: string,
-    age?: number,
-    isActive?: boolean
-}
+import { useState } from "react"
 
-const UserCard: React.FC<TUser> = ({ name, age, isActive = true }) => {
+const Counter: React.FC = () => {
     
+    const [count, setCount] = useState<number>(0)
+
+    const incrementHandler = (): void => { setCount((prevState) => prevState+1) }
+    const decrementHandler = (): void => { if (count == 0) return; setCount((prevState) => prevState-1) }
+    const resetHandler = (): void => { setCount(0) }
+
     return (
-        <div style={{ border: "1px solid #ccc", padding: "10px", width: "250px" }}>
-            <h2>User Card</h2>
-            <p><strong>Name:</strong> {name}</p>
-            {age !== undefined && <p><strong>Age:</strong> {age}</p>}
-            <p><strong>Status:</strong> {isActive ? "Active" : "Inactive"}</p>
+        <div>
+            <div className="flex justify-center mb-10">{count}</div>
+            <button className="px-4 py-2 bg-blue-300 rounded-lg mx-4" onClick={incrementHandler}>+</button>
+            <button className={`px-4 py-2 ${count == 0 ? 'bg-slate-500': 'bg-red-300'} rounded-lg mx-4`} onClick={decrementHandler}>-</button>
+            <button className="px-4 py-2 bg-green-300 rounded-lg mx-4" onClick={resetHandler}>Reset</button>
         </div>
     )
 }
 
-export default UserCard
+export default Counter
